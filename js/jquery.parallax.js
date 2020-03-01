7d4ba0c2f4434399330d245f4d3f67494ac808aa
Giancarlo Delgadillo <giancarlodcwork@gmail.com>
2020-03-01 00:32:19 -0400

    parallaxDos beta 6

--- js/jquery.parallax.js
+++ js/jquery.parallax.js
@@ -3,5 +3,5 @@ $(window).scroll(function(){
   var scroll = $(this).scrollTop();
   $('.parallax1').css({'background-position':'0px '+scroll/5+'px'});
   var scroll1 = $(this).scrollTop();
-  $('.parallax2').css({'background-position':'0px -'+scroll1/8+'px'});
+  $('.parallax2').css({'background-position':'0px -'+scroll1/18+'px'});
 });
