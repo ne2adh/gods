c40ccdc5dba3056cbdfc29aae96f30f0247f99e3
Giancarlo Delgadillo <giancarlodcwork@gmail.com>
2020-03-01 15:06:20 -0400

    parallaxDos oficial

--- js/jquery.parallax.js
+++ js/jquery.parallax.js
@@ -1,7 +1,7 @@
 var $ = jQuery.noConflict();
 $(window).scroll(function(){
   var scroll = $(this).scrollTop();
-  $('.parallax1').css({'background-position':'0px '+scroll/5+'px'});
+  $('.parallax1').css({'background-position':'0px '+scroll/7+'px'});
   var scroll1 = $(this).scrollTop();
   $('.parallax2').css({'background-position':'0px -'+scroll1/18+'px'});
 });
