58d929f53944c94eb3c58a71f114749572059c0b
Giancarlo Delgadillo <giancarlodcwork@gmail.com>
2020-03-02 22:48:49 -0400

    fire velocidad, hover img btn, height parallax

--- js/jquery.parallax.js
+++ js/jquery.parallax.js
@@ -1,7 +1,24 @@
 var $ = jQuery.noConflict();
-$(window).scroll(function(){
+/* $(window).scroll(function(){
 	var scroll = $(this).scrollTop();
-	$('.parallax1').css({'background-position':'0px '+scroll/7+'px'});
+	$('.parallax1').css({'background-position':'0px '+scroll/2+'px'});
 	var scroll1 = $(this).scrollTop();
-	$('.parallax2').css({'background-position':'0px -'+scroll1/18+'px'});
+	$('.parallax2').css({'background-position':'0px -'+scroll1/4+'px'});
 });
+
+
+ */
+  
+  var initScrollTop = $(window).scrollTop();  
+  $('.parallax1').css({'background-position-y' : (initScrollTop/5)+'%'});
+  $('.parallax2').css({'background-position-y' : -(initScrollTop/45)+'%'});
+  
+  $(window).scroll(function() {
+    
+    
+  var scrollTop = $(window).scrollTop();    
+	$('.parallax1').css({'background-position-y' : (scrollTop/5)+'%'});
+	$('.parallax2').css({'background-position-y' : -(scrollTop/45)+'%'});
+	
+    
+  });
